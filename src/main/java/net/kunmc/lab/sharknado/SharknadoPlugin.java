77929832c336ff40ce1c79f24afbc87ef521cb53
Maru32768 <maru32768@gmail.com>
2022-08-16 00:26:53 +0900

    init

--- src/main/java/net/kunmc/lab/sharknado/SharknadoPlugin.java
+++ src/main/java/net/kunmc/lab/sharknado/SharknadoPlugin.java
@@ -1,8 +1,8 @@
-package net.kunmc.lab.paperplugintemplate;
+package net.kunmc.lab.sharknado;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PaperPluginTemplate extends JavaPlugin {
+public final class SharknadoPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
     }
